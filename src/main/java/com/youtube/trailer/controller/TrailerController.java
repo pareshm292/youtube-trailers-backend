b7c86d2bcc442ab65465ad671a14bece66837d56
Manchandia <paresh-shankar.manchandia@dxc.com>
2019-03-10 15:23:56 +0530

    changed key

--- src/main/java/com/youtube/trailer/controller/TrailerController.java
+++ src/main/java/com/youtube/trailer/controller/TrailerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.youtube.trailer.model.Result;
 import com.youtube.trailer.service.OMDbService;
 
-@CrossOrigin("https://morning-hamlet-87493.herokuapp.com/")
+@CrossOrigin("https://tranquil-lowlands-44338.herokuapp.com/")
 @RestController
 public class TrailerController {
 
